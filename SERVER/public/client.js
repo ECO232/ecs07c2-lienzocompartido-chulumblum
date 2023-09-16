f66be3584c14f6ebb0ddc8bf8794d393c1a6bf61
Sebastian Romero <53229969+Nakosume@users.noreply.github.com>
2023-09-16 17:18:15 -0500

    Lienzo Terminado
    Lienzo compartido, con paleta de color y deslizador para el tamaño para que todos los usuarios dibujen a su gusto, hecho en una tarde de viernes sin carullazo, con ligeras modificaciones hechas el sábado siguiente para eliminar líneas de código y variables innecesarias. Si no hice css, fue por pereza.

---
+++ SERVER/public/client.js
@@ -0,0 +1,72 @@
+const socket = io()
+
+let size;
+let col = 0;
+let id = 0;
+let slider
+
+let elements = [];
+let cursors = []
+
+function setup() {
+  createCanvas(400, 400);
+  color = createColorPicker("green");
+  slider = createSlider(0, 40, 20);
+  id = int(random() * 1000)
+  //console.log("id: ", id)
+}
+
+function draw() {
+  background(220);
+  noStroke();
+
+  //draw from elements list
+  elements.forEach((element) => {
+    fill(element.col);
+    ellipse(element.x, element.y, element.size, element.size);
+  })
+
+  //draw from cursors list
+  cursors.forEach((element) => {
+    fill(element.col),
+      ellipse(element.x, element.y, element.size, element.size)
+  })
+}
+
+function mousePressed() {
+  const element = {
+    x: mouseX,
+    y: mouseY,
+    col: color.value(),
+    size
+  }
+
+  socket.emit('send-element', element)
+}
+
+function mouseDragged() {
+  const element = {
+    x: mouseX,
+    y: mouseY,
+    col: color.value(),
+    size: slider.value(),
+    id: id
+  }
+
+  socket.emit('send-cursor', element)
+}
+
+socket.on('element-received', (element) => {
+  //console.log("element-received: ",element)
+  elements.push(element)
+})
+
+socket.on('cursor-received', (element) => {
+  //console.log("element-received: ",element)
+  let cursorIndex = cursors.findIndex((index) => element.id == getItem.id)
+  if (cursorIndex != -1) {
+    cursors[cursorIndex] = element
+  } else {
+    cursors.push(element)
+  }
+})
